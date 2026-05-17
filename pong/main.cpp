6b01b4b3072161beb177e1811effd0a3d3ecc012
Owen Habkirk <owen.habkirk@icloud.com>
2026-05-17 11:08:37 -0700

    Initial Commit

---
+++ pong/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pong
+//
+//  Created by Owen Habkirk on 5/17/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
